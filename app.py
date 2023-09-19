42522fdd0c1578a066b654b4746c62d9660aaa69
shadowik <zahar.mark@bk.ru>
2023-09-20 00:32:30 +0400

    Little design fix

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from io import StringIO
 from flask import Flask, render_template, request
 import pandas as pd
 
+original_table = None
 table = []
 
 def ignore_exception(IgnoreException=Exception, DefaultVal=None):
@@ -17,16 +18,21 @@ def ignore_exception(IgnoreException=Exception, DefaultVal=None):
     return dec
 
 
-def upload_table(data, min_col=-1, max_col=-1, min_row=-1, max_row=-1):
-    global table
-    csv_string_io = StringIO(data)
-    df = pd.read_csv(csv_string_io, sep=",")
+def upload_table(data=None, min_col=-1, max_col=-1, min_row=-1, max_row=-1,):
+    global table, original_table
+    if data:
+        csv_string_io = StringIO(data)
+        df = pd.read_csv(csv_string_io, sep=",")
+    else:
+        df = original_table
 
     min_row = min_row - 1 if min_row else 0
     max_row = max_row if max_row else len(df.index)
 
     min_col = min_col - 1 if min_col else 0
     max_col = max_col if max_col else len(df.columns)
+
+    original_table = df
 
     table = ([df.iloc[min_row:max_row, min_col:max_col].columns.values.tolist()] +
              df.iloc[min_row:max_row, min_col:max_col].values.tolist())
@@ -44,9 +50,13 @@ def hello_world():  # put application's code here
 
     sint = ignore_exception(ValueError)(int)
     if request.method == "POST":
-        file = request.files.get("formFile")
-        file_content = file.read()
-        upload_table(file_content.decode("UTF-8"), sint(request.form["minCol"]), sint(request.form["maxCol"]),
+        try:
+            file = request.files.get("formFile")
+            file_content = file.read().decode("UTF-8")
+        except KeyError:
+            file_content = None
+
+        upload_table(file_content, sint(request.form["minCol"]), sint(request.form["maxCol"]),
                      sint(request.form["minRow"]), sint(request.form["maxRow"]))
         params["data"] = table
         params["start_index"] = sint(request.form["minRow"]) - 1 if sint(request.form["minRow"]) else 0
