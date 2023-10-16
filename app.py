b0f27f4431cb64532cab17c1213d5147229cda77
shadowik <zahar.mark@bk.ru>
2023-10-16 15:25:53 +0400

    Make plot charts + merge them

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from io import StringIO
 
 import pandas
 from flask import Flask, render_template, request
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
+import matplotlib.pyplot as plt
 
 original_table: pd.DataFrame = pd.DataFrame()
+extra_data_table: pd.DataFrame
 table = []
 
 def ignore_exception(IgnoreException=Exception, DefaultVal=None):
@@ -33,24 +37,44 @@ def upload_table(data=None, min_col=-1, max_col=-1, min_row=-1, max_row=-1, new_
     global table, original_table
     if data and new_data:
         csv_string_io = StringIO(data)
-        df = pd.read_csv(csv_string_io, sep=",")
-    else:
-        df = original_table
+        original_table = pd.read_csv(csv_string_io, sep=",")
+
+        add_similar_data()
+        # try:
+        #     original_table.groupby("country")["log_indexprice"].mean().plot(kind="bar", figsize=(10, 5), rot=10)
+        #     plt.savefig("static/myPlot2.png")
+        #     plt.show()
+        #
+        # except Exception as e:
+        #     print(e)
 
     min_row = min_row - 1 if min_row else 0
-    max_row = max_row if max_row else len(df.index)
+    max_row = max_row if max_row else len(original_table.index)
 
     min_col = min_col - 1 if min_col else 0
-    max_col = max_col if max_col else len(df.columns)
+    max_col = max_col if max_col else len(original_table.columns)
 
-    original_table = df
-
-    table_iloc = df.iloc[min_row:max_row, min_col:max_col]
+    table_iloc = original_table.iloc[min_row:max_row, min_col:max_col]
     return format_table(table_iloc)
 
 
-
 def collect_min_max_mean(group_by_column: str, collect_by_column: str):
+    grouped_table = extra_data_table.groupby(group_by_column)[collect_by_column]
+
+    columns = {
+        f"{collect_by_column} min extra": "min",
+        f"{collect_by_column} mean extra": "mean",
+        f"{collect_by_column} max extra": "max"
+    }
+
+    try:
+        ax = grouped_table.agg(**columns).plot(figsize=(10, 5), rot=10)
+        # plt.savefig("static/myPlot2.png")
+        # plt.show()
+
+
+    except Exception as e:
+        print(e)
 
     grouped_table = original_table.groupby(group_by_column)[collect_by_column]
     columns = {
@@ -58,6 +82,15 @@ def collect_min_max_mean(group_by_column: str, collect_by_column: str):
         f"{collect_by_column} mean": "mean",
         f"{collect_by_column} max": "max"
     }
+
+    try:
+        grouped_table.agg(**columns).plot( figsize=(10, 5), rot=10, ax=ax)
+        plt.savefig("static/myPlot.png")
+        plt.show()
+    except Exception as e:
+        print(e)
+
+
     return grouped_table.agg(**columns).sort_values(f"{collect_by_column} max", ascending=False)
 
 
@@ -73,6 +106,7 @@ def collect_min_max_mean_2_col(group_by_column: str, collect_by_column):
 
     }
     return grouped_table.agg(**columns).sort_values(f"{collect_by_column[0]} max", ascending=False)
+
 
 def translate_to_min_max(group_by, collect_by):
     global table
@@ -92,6 +126,22 @@ def translate_to_min_max(group_by, collect_by):
     else:
         res = collect_min_max_mean_2_col(group_by, collect_by).reset_index()
     return format_table(res)
+
+
+def add_similar_data():
+    global original_table, extra_data_table
+    extra_data_table = original_table.copy()
+    count_of_new_lines = int(len(extra_data_table) * 0.10)
+    if count_of_new_lines == 0: return
+    template_of_row: dict = extra_data_table.loc[1].to_dict()
+
+    for key in template_of_row.keys():
+        if is_numeric_dtype(extra_data_table[key]):
+            template_of_row[key] = round(extra_data_table[key].mean(), 2)
+        else:
+            template_of_row[key] = extra_data_table[key].mode()[0]
+
+    extra_data_table = pd.concat([extra_data_table, pd.DataFrame([template_of_row] * count_of_new_lines)], ignore_index=True)
 
 
 app = Flask(__name__)
@@ -131,8 +181,16 @@ def hello_world():  # put application's code here
     params["not_null_el"] = data["not_null_el"]
     params["data"] = table
 
+
+
     return render_template("index.html", **params)
 
 
 if __name__ == '__main__':
+    try:
+        # os.remove("static/myPlot1.png")
+        os.remove("static/myPlot2.png")
+    except:
+        pass
+
     app.run()
